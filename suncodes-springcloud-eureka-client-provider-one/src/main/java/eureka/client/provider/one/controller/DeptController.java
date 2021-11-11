ea255cdf2e3f2bf8c4bc17b12ea9e6d9b92eaa64
sunchuizhe <2605587563@qq.com>
2021-11-11 23:51:39 +0800

    eureka 服务发现

--- suncodes-springcloud-eureka-client-provider-one/src/main/java/eureka/client/provider/one/controller/DeptController.java
+++ suncodes-springcloud-eureka-client-provider-one/src/main/java/eureka/client/provider/one/controller/DeptController.java
@@ -3,6 +3,8 @@ package eureka.client.provider.one.controller;
 import api.entities.Dept;
 import eureka.client.provider.one.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -26,5 +28,24 @@ public class DeptController {
     @RequestMapping(value = "/dept/list", method = RequestMethod.GET)
     public List<Dept> list() {
         return service.list();
+    }
+
+
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery() {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        for (String serviceId : list) {
+            List<ServiceInstance> srvList = client.getInstances(serviceId);
+            for (ServiceInstance element : srvList) {
+                System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                        + element.getUri());
+            }
+        }
+        return this.client;
     }
 }
