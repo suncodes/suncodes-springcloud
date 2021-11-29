6a08c5641d52b47072f4bd2453e7c7d11f949673
sunchuizhe <2605587563@qq.com>
2021-11-29 18:00:09 +0800

    hystrix dashboard + turbine

--- suncodes-springcloud-eureka-client-consumer-feign/src/main/java/eureka/client/consumer/feign/ConsumerFeignApplication.java
+++ suncodes-springcloud-eureka-client-consumer-feign/src/main/java/eureka/client/consumer/feign/ConsumerFeignApplication.java
@@ -1,14 +1,29 @@
 package eureka.client.consumer.feign;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {"eureka.client.consumer.feign",
         "eureka.client.api.feign"})
 @EnableFeignClients(basePackages = "eureka.client.api.feign")
+@EnableHystrixDashboard
 public class ConsumerFeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerFeignApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
     }
 }
---
+++ suncodes-springcloud-eureka-client-hystrix-turbine/src/main/java/eureka/client/hystrix/turbine/HystrixTurbineApplication.java
@@ -0,0 +1,13 @@
+package eureka.client.hystrix.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@SpringBootApplication
+@EnableTurbine
+public class HystrixTurbineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixTurbineApplication.class, args);
+    }
+}
