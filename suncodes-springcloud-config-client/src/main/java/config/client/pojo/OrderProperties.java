3fbaae23bc33d847707da39d81f3622c3223e5af
sunchuizhe <2605587563@qq.com>
2021-12-04 20:26:02 +0800

    config 手动刷新

---
+++ suncodes-springcloud-config-client/src/main/java/config/client/pojo/OrderProperties.java
@@ -0,0 +1,22 @@
+package config.client.pojo;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "order")
+@RefreshScope
+@Data
+public class OrderProperties {
+    /**
+     * 订单支付超时时长，单位：秒。
+     */
+    private Integer payTimeoutSeconds;
+
+    /**
+     * 订单创建频率，单位：秒
+     */
+    private Integer createFrequencySeconds;
+}
