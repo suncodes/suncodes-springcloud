89d9d9c14a30d72f68e27bbf932b1a08b836eed9
sunchuizhe <2605587563@qq.com>
2021-12-09 23:27:07 +0800

    sentinel 流控规则

--- suncodes-springcloud-eureka-client-provider-sentinel/src/main/java/eureka/client/provider/sentinel/service/DeptService.java
+++ suncodes-springcloud-eureka-client-provider-sentinel/src/main/java/eureka/client/provider/sentinel/service/DeptService.java
@@ -3,11 +3,13 @@ package eureka.client.provider.sentinel.service;
 import api.entities.Dept;
 import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import eureka.client.provider.sentinel.dao.DeptDao ;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class DeptService {
     @Autowired
@@ -19,11 +21,24 @@ public class DeptService {
 
     @SentinelResource(value = "get")
     public Dept get(Long id) {
+        log();
         return dao.findById(id);
     }
 
     @SentinelResource(value = "list")
     public List<Dept> list() {
+        log();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            log.error("", e);
+        }
+        log.info("请求部门列表");
         return dao.findAll();
+    }
+
+    @SentinelResource(value = "log")
+    public void log() {
+        log.info("打印日志");
     }
 }
