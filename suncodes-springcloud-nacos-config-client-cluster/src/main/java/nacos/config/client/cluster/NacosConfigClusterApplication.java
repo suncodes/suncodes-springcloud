70a627d268527fd97439f8cea7446c247160331c
sunchuizhe <2605587563@qq.com>
2021-12-22 22:35:49 +0800

    Nacos 集群配置管理

---
+++ suncodes-springcloud-nacos-config-client-cluster/src/main/java/nacos/config/client/cluster/NacosConfigClusterApplication.java
@@ -0,0 +1,11 @@
+package nacos.config.client.cluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NacosConfigClusterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClusterApplication.class, args);
+    }
+}
