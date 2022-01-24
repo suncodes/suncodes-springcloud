b7a3380257c5ff554bc4c113ab35be55627b20ab
sunchuizhe <2605587563@qq.com>
2022-01-25 00:35:50 +0800

    gateway RequestRateLimiter

---
+++ suncodes-springcloud-eureka-gateway/src/main/java/eureka/gateway/RequestRateLimiterConfig.java
@@ -0,0 +1,32 @@
+package eureka.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Configuration
+public class RequestRateLimiterConfig {
+
+//    @Bean
+//    public KeyResolver ipAddressKeyResolver() {
+//        return exchange -> {
+//            String hostAddress = exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
+//            log.info("ipAddressKeyResolver 限流规则 ip: {}", hostAddress);
+//            return Mono.just(hostAddress);
+//        };
+//    }
+
+//    @Bean
+//    public KeyResolver pathKeyResolver() {
+//        return exchange -> {
+//            Route route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
+//            log.info("pathKeyResolver 限流规则 ip: {}", route.getId());
+//            return Mono.just(exchange.getRequest().getPath().toString());
+//        };
+//    }
+}
